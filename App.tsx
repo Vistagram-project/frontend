33313226179c82cc386e97e2849526a3c4790a0d
akashguptawebdev <akashgupta.webdev@gmail.com>
2025-04-14 17:36:18 +0530

    setup bottom nav bar

--- App.tsx
+++ App.tsx
@@ -1,124 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import BottomTab from './src/pages/BottomTab'
 
-import React, { useState } from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  Button,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+ 
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [count , setCount] = useState(0);
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-  const handlePress = ()=>{
+      
+      <BottomTab/>
     
-    setCount(count +1);
-  }
-  const handleMinus = ()=>{
-    if(count<=0) return
-    setCount(count -1);
-  }
-  const safePadding = '5%';
-
-  return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          {/* <Header/> */}
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.white : Colors.black,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-            <Section title='Counter'>
-              <Button title="click" onPress={handleMinus}  color="#841584"></Button>
-              <View>
-                <Text style={{color:"white"}}>{count}</Text>
-              </View>
-              <Button title="click" onPress={handlePress}  color="#841584"></Button>
-            </Section>
-        </View>
-      </ScrollView>
-    </View>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  mainContainer: {
+    backgroundColor: "white",
+    flex: 1,
 
-export default App;
+  },
+  textColor:{
+    color: "red",
+  }
+})
+
+// rnfes
